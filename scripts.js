8d154b40a2bce4d40ebbda5f6d14f9a89ef6f8eb
HZ991 <79825123+HZ991@users.noreply.github.com>
2022-07-30 01:13:20 +1200

    added files

---
+++ scripts.js
@@ -0,0 +1,28 @@
+//Register Modal Controls 
+//javascript code which controls the opening/closing of the register modal 
+
+// Get the modal
+var modal = document.getElementById("joinModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("trigger-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("hz-modal-close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
